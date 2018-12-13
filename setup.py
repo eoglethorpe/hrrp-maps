5f1f094731228223da447d44567280c768bf22e5
Ewan Oglethorpe <oglethorpe.ewan@gmail.com>
2018-12-13 20:54:32 +0100

    made parameters and file paths more flexible for importing

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
-setup(name='hrrp-maps',
+from distutils.core import setup
+
+setup(name='hrrpmaps',
       version='1.0',
       description='code for auto generating maps for HRRP Nepal',
       author='eoglethorpe',
